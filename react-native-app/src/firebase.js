a2300b8db2815ee9846f3393fe93a6dd0c3bfc86
IsaakJG <isaak.j.garside@gmail.com>
2022-05-03 11:44:17 +0100

    inital firebase config

---
+++ react-native-app/src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "./firebase.config";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, db };
