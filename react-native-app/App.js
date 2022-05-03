944a2429333680bf0770d9d1d34f879ede4007cc
IsaakJG <isaak.j.garside@gmail.com>
2022-05-03 12:13:02 +0100

    app.js firebase test

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { StyleSheet, Text, View } from "react-native";
+import { db } from "./src/firebase";
 
 export default function App() {
+  const [queryData, setQueryData] = useState([]);
+
+  useEffect(() => {
+    getDocs(collection(db, "test")).then((data) => {
+      data.docs.forEach((item) => {
+        console.log(item.data());
+        setQueryData((current) => {
+          return [...current, item];
+        });
+      });
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ul key="testDataList">
+        {queryData.map((user, index) => {
+          return (
+            <li key={"testli" + index}>
+              {user.id} === {JSON.stringify(user.data())}
+            </li>
+          );
+        })}
+      </ul>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
