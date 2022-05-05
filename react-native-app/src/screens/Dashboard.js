7679cd43c6f329319698c27fbbee992994886472
good4something-6 <mail6mw@gmail.com>
2022-05-05 12:29:47 +0100

    user details retrieved on dashboard

--- react-native-app/src/screens/Dashboard.js
+++ react-native-app/src/screens/Dashboard.js
@@ -3,29 +3,31 @@ import ProfileDetailsBtn from "../components/ProfileDetailsBtn";
 import SearchGymsBtn from "../components/SearchGymsBtn";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase";
+import { useEffect, useState } from "react";
 
 const Dashboard = ({ route }) => {
   let userEmail = route.params.userEmail;
- 
-  let userInfo = [];
 
-  const colRef = collection(db, "users");
-  getDocs(colRef)
-    .then((snapshot) => {
-      
+  const [userInfo, setUserInfo] = useState({});
 
-      snapshot.docs.forEach((doc) => {
-        if (doc.data().email === userEmail) {
-          userInfo.push({ ...doc.data(), id: doc.id });
-        }
-      });
-    })
-    .catch((err) => console.log(err));
+  useEffect(() => {
+    const colRef = collection(db, "users");
+    getDocs(colRef)
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if (doc.data().email === userEmail) {
+            setUserInfo({ ...doc.data(), id: doc.id });
+          }
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    console.log(userInfo);
+  console.log(userInfo.username);
+
   return (
     <div>
-      <h1>Welcome </h1>
+      <h1>Welcome {userInfo.username}</h1>
       <br></br>
       <ProfileDetailsBtn />
       <SearchGymsBtn />
