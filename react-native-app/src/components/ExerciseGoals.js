575188c066e20f8ea18e0da16f78156219386c74
good4something-6 <mail6mw@gmail.com>
2022-05-10 16:15:48 +0100

    change status of exercise goals is working

--- react-native-app/src/components/ExerciseGoals.js
+++ react-native-app/src/components/ExerciseGoals.js
@@ -1,4 +1,11 @@
-import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+import {
+  collection,
+  connectFirestoreEmulator,
+  doc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+
 import { db } from "../firebase";
 import { useEffect, useState, useContext } from "react";
 import {
@@ -6,6 +13,7 @@ import {
   View,
   Text,
   TextInput,
+  CheckBox,
   TouchableOpacity,
 } from "react-native";
 
@@ -15,8 +23,6 @@ const ExerciseGoals = () => {
   const [exerciseGoals, setExerciseGoals] = useState([]);
   const [currentGoalentry, setCurrentGoalentry] = useState("");
   const { user, setUser } = useContext(UserContext);
-
-  //
 
   useEffect(() => {
     const colRef = collection(db, "userExerciseGoals");
@@ -46,7 +52,11 @@ const ExerciseGoals = () => {
   const saveGoalClick = () => {
     if (currentGoalentry !== "") {
       setExerciseGoals((currExerciseGoals) => {
-        return [...currExerciseGoals, currentGoalentry];
+        // return [...currExerciseGoals, currentGoalentry];
+        return [
+          ...currExerciseGoals,
+          { goal: currentGoalentry, completed: false },
+        ];
       });
       setCurrentGoalentry("");
     }
@@ -71,18 +81,69 @@ const ExerciseGoals = () => {
         </TouchableOpacity>
       </View>
       <Text style={styles.textSubTitle}>Your Goals</Text>
+      <Text style={styles.goalListItem}>
+        Tick a box to mark a goal as complete
+      </Text>
       <View>
         {exerciseGoals.map((goal, ind) => {
-          return (
-            <Text style={styles.goalListItem} key={ind}>
-              {goal}
-            </Text>
-          );
+          if (!goal.completed) {
+            return (
+              <View key={"goalview" + ind}>
+                <Text style={styles.goalListItem} key={"goalkey" + ind}>
+                  <CheckBox
+                    disabled={false}
+                    value={goal.completed}
+                    onValueChange={(newValue) => {
+                      let modGoals = exerciseGoals.map((ele, eleInd) => {
+                        if (eleInd !== ind) {
+                          return ele;
+                        } else {
+                          return { ...ele, completed: newValue };
+                        }
+                      });
+                      setExerciseGoals(modGoals);
+                    }}
+                  />
+                  {"   " + goal.goal}
+                </Text>
+              </View>
+            );
+          }
+        })}
+      </View>
+      <Text style={styles.textSubTitle}>Completed Goals</Text>
+      <View>
+        {exerciseGoals.map((goal, ind) => {
+          if (goal.completed) {
+            return (
+              <View key={"goalview" + ind}>
+                <Text style={styles.goalListItem} key={ind}>
+                  <CheckBox
+                    disabled={false}
+                    value={goal.completed}
+                    onValueChange={(newValue) => {
+                      let modGoals = exerciseGoals.map((ele, eleInd) => {
+                        if (eleInd !== ind) {
+                          return ele;
+                        } else {
+                          return { ...ele, completed: newValue };
+                        }
+                      });
+                      setExerciseGoals(modGoals);
+                    }}
+                  />
+                  {"   " + goal.goal}
+                </Text>
+              </View>
+            );
+          }
         })}
       </View>
     </>
   );
 };
+
+// {goal: "walk", completed: false}
 
 const styles = StyleSheet.create({
   text: {
